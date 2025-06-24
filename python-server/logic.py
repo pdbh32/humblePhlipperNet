d9f2b7c6334ac0965507bf0ded247126f3a903c6
pdbh32 <pdbhartfield@gmail.com>
2025-06-24 23:49:12 +0100

    Still bugged, undid for now

--- python-server/logic.py
+++ python-server/logic.py
@@ -54,8 +54,8 @@ def check_cancel(data, prices):
     for offer in data.get("portfolio", {}).get("offerList", []):
         if offer.get("status", "EMPTY") == "EMPTY" or offer["readyToCollect"]: continue
         if (
-            (offer["status"] == "BUY" and any(other_offer["itemId"] == offer["itemId"] and other_offer["slotIndex"] != offer["slotIndex"] for other_offer in data["portfolio"]["offerList"])) # already have an offer for this item
-            or
+            # (offer["status"] == "BUY" and any(other_offer["itemId"] == offer["itemId"] and other_offer["slotIndex"] != offer["slotIndex"] for other_offer in data["portfolio"]["offerList"])) # already have an offer for this item
+            # or
             (offer["status"] == "BUY" and offer["price"] != prices.get(offer["itemId"], {}).get("bid", 0)) # price mismatch
             or
             (offer["status"] == "SELL" and offer["price"] != prices.get(offer["itemId"], {}).get("ask", offer["price"])) # price mismatch
