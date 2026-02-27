6d66a1066a91ad0debbcb371dc17dea76fd30634
pdbh32 <26061689+pdbh32@users.noreply.github.com>
2026-02-27 11:12:19 +0000

    Small refactor

--- python-server/humblePhlipperPython/services/scheduler.py
+++ python-server/humblePhlipperPython/services/scheduler.py
@@ -8,8 +8,8 @@ from typing import Callable
 
 from humblePhlipperPython.config import settings
 from humblePhlipperPython.config.runtime import SESSION_TIMESTAMP
-from humblePhlipperPython.caches import market_data as wiki_cache, quotes as quotes_cache
-from humblePhlipperPython.storage import market_data as wiki_storage, events as events_storage
+from humblePhlipperPython.caches import market_data as market_data_cache, quotes as quotes_cache
+from humblePhlipperPython.storage import market_data as market_data_storage, events as events_storage
 from humblePhlipperPython.integrations import wiki, discord
 from humblePhlipperPython.utils import profit_calculator
 from humblePhlipperPython.core.base import BaseQuoteModel
@@ -38,34 +38,34 @@ def _load_quote_model() -> BaseQuoteModel:
 QUOTE_MODEL = _load_quote_model()
 
 def _ingest_wiki_data(endpoint: str, timestamp: int) -> None:
-    if wiki_storage.get_path(endpoint, timestamp).exists(): return wiki_storage.load(endpoint, timestamp)
+    if market_data_storage.get_path(endpoint, timestamp).exists(): return market_data_storage.load(endpoint, timestamp)
     return wiki.fetch(endpoint, timestamp)
 
 def _update_wiki_cache(endpoint: str, int_secs: int, T: int) -> None:
-    existing_ts = wiki_cache.get(endpoint).keys()
+    existing_ts = market_data_cache.get(endpoint).keys()
     required_ts = [int(time.time() // int_secs - t) * int_secs for t in range(T, 0, -1)] if endpoint in ["5m", "1h"] else (sorted(existing_ts)[-(T-1):] if T > 1 else []) + [int(time.time())]
     stale_ts    = [ts for ts in existing_ts if ts not in required_ts]
     missing_ts  = [ts for ts in required_ts if ts not in existing_ts]
     new_entries = {ts: _ingest_wiki_data(f"{endpoint}", timestamp=ts) for ts in missing_ts}
 
     for ts in stale_ts:
-        wiki_cache.pop(endpoint, ts)
+        market_data_cache.pop(endpoint, ts)
     for ts in missing_ts:
-        wiki_cache.set(endpoint, new_entries[ts]["timestamp"], new_entries[ts])
+        market_data_cache.set(endpoint, new_entries[ts]["timestamp"], new_entries[ts])
         if endpoint != "mapping":
-            wiki_storage.save(new_entries[ts])
+            market_data_storage.save(new_entries[ts])
 
 def _init_quotes_cache() -> None:
-    five_m = wiki_cache.get_df("5m")
-    one_h = wiki_cache.get_df("1h")
-    latest = wiki_cache.get_df("latest")
+    five_m = market_data_cache.get_df("5m")
+    one_h = market_data_cache.get_df("1h")
+    latest = market_data_cache.get_df("latest")
     quotes = QUOTE_MODEL.train(five_m, one_h, latest)
     quotes_cache.update(quotes)
 
 def _update_quotes_cache() -> None:
-    five_m = wiki_cache.get_df("5m")
-    one_h = wiki_cache.get_df("1h")
-    latest = wiki_cache.get_df("latest")
+    five_m = market_data_cache.get_df("5m")
+    one_h = market_data_cache.get_df("1h")
+    latest = market_data_cache.get_df("latest")
     quotes = QUOTE_MODEL.update(five_m, one_h, latest)
     quotes_cache.update(quotes)
 
